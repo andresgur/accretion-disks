fa44a211482afb153459efed6fc93a344db638f8
andresgur <andress.gurpide@gmail.com>
2025-11-17 11:09:32 +0100

    Fixed input overriding parent base class

--- accretion_disks/shakurasunyaevdisk.py
+++ accretion_disks/shakurasunyaevdisk.py
@@ -1,12 +1,12 @@
-from basedisk import Disk
+from .basedisk import Disk
 from math import pi
-from constants import ccgs, k_T
+from .constants import ccgs, k_T
 
 class ShakuraSunyaevDisk(Disk):
 
-    def __init__(self, CO, mdot, alpha=0.1, name="Shakura-Sunyaev Disk", Rmin=1, Rmax=500, N=1000, torque_Rmin=0):
-        super().__init__(CO, mdot, alpha, name, Rmin, Rmax, N)
-        self.torque_Rmin = 0  # Torque at the inner boundary
+    def __init__(self, *args, name="Shakura-Sunyaev Disk", torque_Rmin=0., **kwargs):
+        super().__init__(*args, name=name, **kwargs)
+        self.torque_Rmin = torque_Rmin  # Torque at the inner boundary
 
     def height(self, Wrphi):
         """
@@ -22,10 +22,15 @@ class ShakuraSunyaevDisk(Disk):
 
     def torque(self, R):
         """Analytical expression for the torque when Mass loss is conserved
-        and assuming the torque at the inner boundary is 0
         """
-        Rmin = R[0]
+        Rmin = self.Rmin * self.CO.Risco
         return -(self.Mdot_0 * self.Omega / (2. * pi) * (1 - (Rmin / R)**0.5) + self.torque_Rmin * (Rmin / R)**2.)
+    
+
+    def torque_derivative(self, R):
+        """Derivative of the Torque"""
+        Rmin = self.Rmin * self.CO.Risco
+        return -(self.Mdot_0 * self.Omega / (4 * pi * R) * (4 * (Rmin / R)**0.5 - 3) - 2 * self.torque_Rmin * (Rmin)**2 / (R**3))
 
 
     def solve(self):
