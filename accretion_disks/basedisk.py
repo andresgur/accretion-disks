67841c5080d107909069953fd666c75fa58f0e74
andresgur <andress.gurpide@gmail.com>
2025-11-19 16:47:41 +0100

    Defined non advective disk class

--- accretion_disks/basedisk.py
+++ accretion_disks/basedisk.py
@@ -9,14 +9,17 @@ class Disk(ABC):
         self.CO = CO
         self.mdot = mdot
         self.alpha = alpha
-        self.Mdot_0 = self.CO.Medd * self.mdot
+        self.Mdot_0 = self.CO.MEdd * self.mdot
         self.name = name
-        print("Disk %s with M = %.1f M_sun, dot(m) = %.1f and alpha = %.1f and spin = %.1f" % (self.name, self.CO.M / M_suncgs,
-                      self.mdot, self.alpha, self.CO.a))
+        print("Disk %s with M = %.1f M_sun, dot(m) = %.1f and alpha = %.1f and spin = %.1f and N = %d datapoints" % (self.name, self.CO.M / M_suncgs,
+                      self.mdot, self.alpha, self.CO.a, N))
         self.Rmin = Rmin
         self.Rmax = Rmax
         self.R = np.linspace(self.Rmin, self.Rmax, N) * self.CO.Risco
+        self.N = N
         self.Omega = self.CO.omega(self.R)
+        self.Mdot = self.Mdot_0 * np.ones(self.N)
+
 
   
     def density(self, Wrphi, H):
@@ -31,6 +34,18 @@ class Disk(ABC):
             Keplerian angular velocity
         """
         return -Wrphi / (2 * self.alpha * self.Omega**2. * H**3.)
+    
+
+    def pressure(self, H, rho):
+        """Returns the (radiation) pressure of the disk
+        Parameters
+        ----------
+        H: float
+            Height
+        rho: float
+            Density
+        """
+        return self.Omega**2. * H**2 * rho
 
 
     def Q_rad(self, H):
@@ -54,6 +69,38 @@ class Disk(ABC):
         """
         return Mdot / (rho * H * 4 * pi * R)
     
+    def L(self, Rmin=None, Rmax=None):
+        deltaR = self.R[1] - self.R[0]
+        if Rmin is None:
+            Rmin = self.Rmin * self.CO.Risco
+        if Rmax is None:
+            Rmax = self.Rmax * self.CO.Risco
+        # Fix: select radii between Rmin and Rmax
+        R_range = (self.R >= Rmin) & (self.R <= Rmax)
+        L = 4 * pi * (self.R[R_range] * self.Qrad[R_range]).sum() * deltaR
+        return L
+
     @abstractmethod
     def solve():
         pass
+class NonAdvectiveDisk(Disk):
+    """Base class for non-advective disks
+    """
+
+    def __init__(self, *args, name="NonAdvectiveDisk", **kwargs):
+        super().__init__(*args, name=name, **kwargs)
+        self.Qadv = np.zeros_like(self.R)
+        
+    def height(self, Wrphi):
+        """
+        Disk height. Inputs in cgs units. From Equation 13 in the pdf
+
+        Parameters
+        ----------
+        Wrphi: float or array-like
+            The torque at each radius in cgs units
+        """
+        # 3/4 = 0.75
+        H = - 0.75 * k_T * Wrphi / (self.Omega * ccgs)
+        
+        return H
