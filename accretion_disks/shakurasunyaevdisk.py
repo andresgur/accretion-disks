534668d7997b87e76eae29a5c4f9f6a682577221
andresgur <andress.gurpide@gmail.com>
2025-11-19 16:48:10 +0100

    Added more variables, improved naming of inner torque

--- accretion_disks/shakurasunyaevdisk.py
+++ accretion_disks/shakurasunyaevdisk.py
@@ -1,36 +1,24 @@
-from .basedisk import Disk
+from .basedisk import NonAdvectiveDisk
 from math import pi
-from .constants import ccgs, k_T
+from .constants import A
 
-class ShakuraSunyaevDisk(Disk):
+class ShakuraSunyaevDisk(NonAdvectiveDisk):
 
-    def __init__(self, *args, name="Shakura-Sunyaev Disk", torque_Rmin=0., **kwargs):
+    def __init__(self, *args, name="Shakura-Sunyaev Disk", Wrphi_in=0., **kwargs):
         super().__init__(*args, name=name, **kwargs)
-        self.torque_Rmin = torque_Rmin  # Torque at the inner boundary
-
-    def height(self, Wrphi):
-        """
-        Disk height. Inputs in cgs units
-
-        Parameters
-        ----------
-        """
-        # 3/4 = 0.75
-        H = - 0.75 * k_T * Wrphi / (self.Omega * ccgs)
-        return H
-    
+        self.Wrphi_in = Wrphi_in    
 
     def torque(self, R):
         """Analytical expression for the torque when Mass loss is conserved
         """
         Rmin = self.Rmin * self.CO.Risco
-        return -(self.Mdot_0 * self.Omega / (2. * pi) * (1 - (Rmin / R)**0.5) + self.torque_Rmin * (Rmin / R)**2.)
+        return -(self.Mdot_0 * self.Omega / (2. * pi) * (1 - (Rmin / R)**0.5) + self.Wrphi_in * (Rmin / R)**2.)
     
 
     def torque_derivative(self, R):
         """Derivative of the Torque"""
         Rmin = self.Rmin * self.CO.Risco
-        return -(self.Mdot_0 * self.Omega / (4 * pi * R) * (4 * (Rmin / R)**0.5 - 3) - 2 * self.torque_Rmin * (Rmin)**2 / (R**3))
+        return -(self.Mdot_0 * self.Omega / (4 * pi * R) * (4 * (Rmin / R)**0.5 - 3) - 2 * self.Wrphi_in * (Rmin)**2 / (R**3))
 
 
     def solve(self):
@@ -39,3 +27,5 @@ class ShakuraSunyaevDisk(Disk):
         self.Qrad = self.Q_rad(self.H)
         self.rho = self.density(self.Wrphi, self.H)
         self.vr = self.v_r(self.Mdot_0, self.H, self.rho, self.R)
+        self.P = self.pressure(self.H, self.rho)
+        self.T = (3 * self.P / A)** (1/4)
