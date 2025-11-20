5cc7202e62ddede6cb59d261b54ae9cce130e243
andresgur <andress.gurpide@gmail.com>
2025-11-20 14:13:16 +0100

    Fixed problem with Rsph, added CompositeDisk to handle outer and inner disks. Included ODE and analytical disks with outflows

--- accretion_disks/diskwithoutflows.py
+++ accretion_disks/diskwithoutflows.py
@@ -1,10 +1,9 @@
 from .basedisk import NonAdvectiveDisk
 from .shakurasunyaevdisk import ShakuraSunyaevDisk
 from math import pi
-from .constants import k_T, A, ccgs, Gcgs
+from .constants import A, Gcgs
 from scipy.integrate import solve_bvp
 import numpy as np
-import warnings
 
 
 
@@ -28,7 +27,7 @@ def mass_transfer_inner_radius(m_0, e_wind=0.5):
     return (1 - a) / (1 - a * (2/5 * m_0) ** (- 0.5) )
 
 
-class DiskWithOutflows(NonAdvectiveDisk):
+class DiskWithOutflowsRemove(NonAdvectiveDisk):
     def __init__(self, *args, name="Disk With Outflows", Wrphi_in=0, ewind=1, **kwargs):
         super().__init__(*args, name=name, **kwargs)
         self.Wrphi_in = Wrphi_in
@@ -80,55 +79,45 @@ class DiskWithOutflows(NonAdvectiveDisk):
                 L_Ra = L_Rc
         return None
     
+    def outerTorque(self, R, Wrphin, Rmin):    
 
+        Wphi = -(self.Mdot_0 * self.Omega / (2. * pi) * (1 - (Rmin / R)**0.5) + Wrphin * (Rmin / R)**2.)
+        return Wphi
 
     def torque(self, R):
+        """Calculate the torque at a given radius by joining the inner and outer SS73 disks solutions
+        Parameters
+        ----------
+        R: float or array-like
+            The radius at which to calculate the torque.
+        Returns
+        -------
+        float or array-like
+            The calculated torque.
+        """
+        WrphiRsph = self.innertorque(self.Rsph)
+        Wrphi = np.where(R > self.Rsph, self.outerTorque(R, -WrphiRsph, self.Rsph), self.innertorque(R))
+        return Wrphi
+
+
+    def innertorque(self, R):
         """Calculate the torque at a given radius.
 
         Parameters
         ----------
-        R: float
+        R: float or array-like
             The radius at which to calculate the torque.
 
         Returns
         -------
-        float
+        float or array-like
             The calculated torque.
         """
         # 2.5 = 5/2
         # 3/2 = 1.5
         Rmin = self.Rmin * self.CO.Risco
-        return self.Mdot * self.Omega / (4. * np.pi) * (1. - (R / Rmin)**2.5) / (1. + 1.5 * (R / Rmin)**2.5)
-    
-    def _gR(self, R):
-        Rmin = self.Rmin * self.CO.Risco
-        r = R / Rmin
-        rsph = self.Rsph / Rmin
-        gr = self.mdot /3 * r**(3/2) / rsph * (1 - r**(-5/2))/ (1 + 2/3 * rsph**(-5/2))
-        return gr
-
-    def torquePoutanen(self, R):
-        """Analytical expression for the torque when mass loss is included.
-
-        Parameters
-        ----------
-        R: float
-            The radius at which to calculate the torque.
-
-        Returns
-        -------
-        float
-            The calculated torque based on Poutanen+2007.
-        """
-        
-        #
-        Rmin = self.Rmin * self.CO.Risco
-        g0 = (Gcgs * self.CO.M * Rmin)**0.5 * self.CO.MEdd
-        grsph = self._gR(self.Rsph)
-        r = R / Rmin
-        gr = np.where(self.R > self.Rsph, grsph / g0 + self.mdot  * (r**0.5 - (self.Rsph / self.R)**0.5), self._gR(self.R)) * g0
-        Wrphi = gr / (2 * np.pi * self.R**2)
-        return Wrphi
+        Wrphiinner = self.Mdot * self.Omega / (4. * np.pi) * (1. - (R / Rmin)**2.5) / (1. + 1.5 * (R / Rmin)**2.5)
+        return Wrphiinner
     
     
     def Mdot_R(self, R):
@@ -161,73 +150,63 @@ class DiskWithOutflows(NonAdvectiveDisk):
         self.T = (3 * self.P / A)** (1/4)
 
 
-
-class DiskWithOutflowsODE(NonAdvectiveDisk):
-    def __init__(self, *args, name="Disk With Outflows", Wrphi_in=0, ewind=1, **kwargs):
+class InnerDisk(NonAdvectiveDisk):
+    def __init__(self, *args, name="Inner Disk With Outflows", Wrphi_in=0, ewind=1, **kwargs):
         super().__init__(*args, name=name, **kwargs)
         self.Wrphi_in = Wrphi_in
         self.ewind = ewind
-        # Approximative solution for Rpsh based on Poutanen+2007
-        deltaR = np.diff(self.R)[0]
-        disk = ShakuraSunyaevDisk(*args, name="Temporary SS Disk", **kwargs)
-        disk.solve()
-        Qrad = disk.Qrad
-        Ltot = 4 * np.pi * np.sum(Qrad * self.R) * deltaR
-        if Ltot < self.CO.LEdd:
-            warnings.warn("Spherization radius is not defined as the disk does not exceed its Eddington limit. Either you need to increase the outer radius, or the mass-transfer rate")
-            # set disk to Shakura-Sunyaev
-            self.Rsph = self.Rmax * self.CO.Risco
-        else:
-            self.Rsph = 1.62 * self.mdot * self.CO.Risco
-        
-        # For BVP solver with integral constraint
-        self._current_x = None
-        self._current_y = None
-        self.Ledd = self.CO.LEdd        
-        
 
-    def get_spherization_radius(self, Qrad, maxiter=100, reltol=1e-8):
-        """Calculate the spherization radius based on the radiative flux.
+    def torque(self, R):
+        """Calculate the torque at a given radius.
 
         Parameters
         ----------
-        Qrad: array-like
-            Radiative flux at different radii.
-        maxiter: int, optional
-            Maximum number of iterations for the solver.
-        reltol: float, optional
-            Relative tolerance for convergence.
+        R: float or array-like
+            The radius at which to calculate the torque.
 
         Returns
         -------
-        float or None
-            The calculated spherization radius or None if not found.
+        float or array-like
+            The calculated torque.
         """
-        Ra = self.R[0]
-        Rb = self.R[-1]
-        deltaR = self.R[1] - self.R[0]
-        R_range = (self.R > Ra)
-        fourpideltaR = 4 * np.pi * deltaR
-        QradR = Qrad * self.R
-        L_Ra = fourpideltaR * QradR[R_range].sum() - self.CO.LEdd
-        print("Full luminosity", fourpideltaR * QradR[R_range].sum() / self.CO.LEdd)
+        # 2.5 = 5/2
+        # 3/2 = 1.5
+        Rmin = self.Rmin * self.CO.Risco
+        Wrphi = self.Mdot * self.Omega / (4. * np.pi) * (1. - (R / Rmin)**2.5) / (1. + 1.5 * (R / Rmin)**2.5)
+        return Wrphi
 
-        for i in range(maxiter):
-            R_c = (Ra + Rb) / 2.0
-            # integrate outer radii
-            R_range = (self.R > R_c)
-            L_Rc = fourpideltaR * QradR[R_range].sum() - self.CO.LEdd
-            err = abs(R_c - Ra)
-            if err / R_c < reltol or L_Rc == 0:
-                print(f"Solution found after {i+1} iterations with error {err:.2e}")
-                return R_c
-            if (L_Rc * L_Ra) < 0:
-                Rb = R_c
-            else:
-                Ra = R_c
-                L_Ra = L_Rc
-        return None
+    
+    def Mdot_R(self, R):
+        """Calculate the mass-transfer rate at a given radius.
 
+        Parameters
+        ----------
+        R: float
+            The radius at which to calculate the mass-transfer rate.
+
+        Returns
+        -------
+        float
+            The mass-transfer rate at the given radius.
+        """
+        Rmin = self.Rmin * self.CO.Risco
+        Rsph = self.Rmax * self.CO.Risco
+        Mdot = self.Mdot_0 * (Rsph / R)**1.5 * (1 + 1.5 * (R / Rmin)**2.5) / (1 + 1.5 * (Rsph / Rmin)**2.5)
+        return Mdot
+
+
+    def solve(self, **kwargs):
+        self.Mdot = self.Mdot_R(self.R)
+        self.Wrphi = self.torque(self.R)
+        self.H = self.height(self.Wrphi)
+        self.Qrad = self.Q_rad(self.H)
+
+
+class InnerDiskODE(NonAdvectiveDisk):
+    def __init__(self, *args, name="Inner Disk With Outflows", Wrphi_in=0, ewind=1, **kwargs):
+        super().__init__(*args, name=name, **kwargs)
+        self.Wrphi_in = Wrphi_in
+        self.ewind = ewind
 
     def _torque_derivative(self, Mdot, Wrphi, R):
         """Derivative of the torque
@@ -245,7 +224,6 @@ class DiskWithOutflowsODE(NonAdvectiveDisk):
         """
         return -(Mdot * self.CO.omega(R) / (4 * pi)  + 2 * Wrphi) / R
 
-
     def Mdotprime(self, Wrphi, R):
         """From equation 12 and replacing Qrad using Equation 8 and the Pressure using Equation 9
         The derivative of Mdot is positive (it increases as we move outward), because Wrphi is negative, here a negative sign must be kept (it also follows from the derivation)
@@ -259,16 +237,12 @@ class DiskWithOutflowsODE(NonAdvectiveDisk):
         """
         return -6. * np.pi * Wrphi / (self.CO.omega(R) * R)
 
-
-
     def bc(self, ya, yb):
 
         return np.array([
             yb[0] - self.Mdot_0,      # Mdot(Rsph) = Mdot_0
             ya[1] - self.Wrphi_in,   # W_rphi(Rmin) = Wrphi_in
         ])
-
-
 
     def ode(self, x, y):
         """ODE system for BVP solver with parameter p=[Rsph]"""
@@ -282,14 +256,16 @@ class DiskWithOutflowsODE(NonAdvectiveDisk):
         # Convert to derivatives wrt x and return as 2D array
         return np.vstack([dMdot_dR, dWrphi_dR])
 
-
     def solve(self, **kwargs):
         """Solve the coupled ODEs with BVP solver and integral constraint."""
+
         # Initial guess for [Mdot, Wrphi]
-        M_isco = mass_transfer_inner_radius(self.mdot, self.ewind) * self.Mdot_0
-        Mdot_guess = M_isco + (self.Mdot_0 - M_isco)  * (self.R / self.Rmin) / self.Rsph
+        Rsph = self.Rmax * self.CO.Risco 
         Rmin = self.Rmin * self.CO.Risco
-        Wrphi_guess = -self.Mdot_0 * self.R / (self.Rsph * Rmin) * self.Omega / (4 * np.pi) * (1 - (self.R / Rmin)**(5/2)) / (1 + 3/2 * (self.R / Rmin)**(5/2))
+        M_isco = mass_transfer_inner_radius(self.mdot, self.ewind) * self.Mdot_0
+        Mdot_guess = M_isco + (self.Mdot_0 - M_isco)  * (self.R / Rmin) / Rsph 
+        #Mdot_guess = self.Mdot_0 * self.R / (self.Rmax * self.CO.Risco)
+        Wrphi_guess = -self.Mdot_0 * self.R / (Rsph * Rmin) * self.Omega / (4 * np.pi) * (1 - (self.R / Rmin)**(5/2)) / (1 + 3/2 * (self.R / Rmin)**(5/2))
         Wrphi_guess[0] = self.Wrphi_in
 
         initial_guess = np.array([Mdot_guess, Wrphi_guess])
@@ -303,7 +279,87 @@ class DiskWithOutflowsODE(NonAdvectiveDisk):
         self.Wrphi = solution[1]
         self.H = self.height(self.Wrphi)
         self.Qrad = self.Q_rad(self.H)
+
+
+
+class CompositeDisk(NonAdvectiveDisk):
+    """Base class for disks with different inner and outer solutions.
+    Extends NonAdvectiveDisk, but requires innerDiskClass and optionally outerDiskClass (default: ShakuraSunyaevDisk).
+    """
+    def __init__(self, innerDiskClass, outerDiskClass=ShakuraSunyaevDisk, *args, name="CompositeDisk", Wrphi_in=0, ewind=1, **kwargs):
+        super().__init__(*args, name=name, **kwargs)
+        self.innerDiskClass = innerDiskClass
+        self.outerDiskClass = outerDiskClass
+        self.Wrphi_in = Wrphi_in
+        self.ewind = ewind
+        self.Rsph = None
+
+    
+    def adjust_Rsph(self, maxiter=100, reltol=1e-4,):
+        """Calculate the spherization radius based on the radiative flux.
+
+        Parameters
+        ----------
+        maxiter: int, optional
+            Maximum number of iterations for the solver.
+        reltol: float, optional
+            Relative tolerance for convergence.
+
+        Returns
+        -------
+        float or None
+            The calculated spherization radius or None if not found.
+        innerDisk: InnerDisk
+            The solved inner disk object whithin Rsph
+        outerDisk: ShakuraSunyaevDisk
+            The solved outer disk object, which is a SS73 disk with modified boundary conditions.
+        """
+        Ra = self.R[0] // self.CO.Risco
+        Rb = self.R[-1] // self.CO.Risco
+
+        outerdisk = self.outerDiskClass(self.CO, self.mdot, self.alpha, Rmax=self.Rmax, Rmin=self.Rmin, N=self.N, 
+                                       name="Temporary SS Disk", Wrphi_in=0)
+        outerdisk.solve()
+        L_Ra = outerdisk.L() - self.CO.LEdd
+
+        for i in range(maxiter):
+            R_c = (Ra + Rb) / 2.0
+            Ninner = (self.R <= R_c * self.CO.Risco).sum()
+            # reset the maximum radius
+            innerDisk = self.innerDiskClass(self.CO, self.mdot, self.alpha, Rmin=self.Rmin, Rmax=R_c, N=Ninner, 
+                                  name="Inner Disk", Wrphi_in=self.Wrphi_in, 
+                                  ewind=self.ewind)
+            innerDisk.solve()
+            Nouter = self.N - Ninner
+            if Nouter ==0:
+                raise ValueError("Run out of points in Rsph calculation. Increase the number of grid points!")
+            # create truncated SS73 with new Wrphi boundary condition
+            outerDisk = self.outerDiskClass(self.CO, self.mdot, self.alpha, Rmin=R_c,
+                                           Rmax=self.Rmax, N=Nouter, name="Outer Disk", 
+                                           Wrphi_in=-innerDisk.Wrphi[-1])
+            outerDisk.solve()
+            L_Rc = outerDisk.L() - self.CO.LEdd
+            err = abs(R_c - Ra)
+            if err / R_c < reltol or L_Rc == 0:
+                print(f"Solution found after {i+1} iterations with error {err:.2e}")
+                return R_c, innerDisk, outerDisk
+            if (L_Rc * L_Ra) < 0:
+                Rb = R_c
+            else:
+                Ra = R_c
+                L_Ra = L_Rc
+        return None
+    
+    def solve(self):
+
+        Rsph, self.innerDisk, self.outerDisk = self.adjust_Rsph()
+        self.Rsph = Rsph * self.CO.Risco
+        # Combine solutions
+        self.Mdot = np.concatenate((self.innerDisk.Mdot, self.outerDisk.Mdot))
+        self.Wrphi = np.concatenate((self.innerDisk.Wrphi, self.outerDisk.Wrphi))
+        self.H = self.height(self.Wrphi)
+        self.Qrad = self.Q_rad(self.H)
         self.rho = self.density(self.Wrphi, self.H)
-        self.v_r = self.v_r(self.Mdot, self.H, self.rho, self.R)
+        self.vr = self.v_r(self.Mdot, self.H, self.rho, self.R)
         self.P = self.pressure(self.H, self.rho)
         self.T = (3 * self.P / A) ** (1/4)
--- tests/diskwithoutflows_test.py
+++ tests/diskwithoutflows_test.py
@@ -1,5 +1,5 @@
 import unittest
-from accretion_disks.diskwithoutflows import DiskWithOutflows, DiskWithOutflowsODE
+from accretion_disks.diskwithoutflows import CompositeDisk, InnerDiskODE, InnerDisk, DiskWithOutflowsRemove
 from accretion_disks.shakurasunyaevdisk import ShakuraSunyaevDisk
 from accretion_disks.compact_object import CompactObject
 from accretion_disks.constants import Gcgs
@@ -7,43 +7,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class TestDiskWithOutflows(unittest.TestCase):
+    def testManualRsph(self):
+        blackhole = CompactObject(M=10, a=0)
+        mdot = 100
+        for diskclass in [InnerDiskODE, InnerDisk]:
+            disk = diskclass(blackhole, mdot=mdot, alpha=0.1, Rmax=1.62 * mdot, )
+            disk.solve()
+            ss73 = ShakuraSunyaevDisk(blackhole, mdot=mdot, alpha=0.1, Rmax=10000, Rmin=1.62 * mdot, Wrphi_in=-disk.Wrphi[-1])
+            ss73.solve()
+            self.assertAlmostEqual(ss73.L() / blackhole.LEdd, 1, delta=0.1, msg="Modified SS73 does not produce Eddington luminosity!")
 
+    def testODE(self):
+        """Test that the disk solving the differential equations yields the same results as the analytical one"""
+        blackhole = CompactObject(M=10, a=0)
+        for mdot in np.arange(10, 100, 10):
+            analyticalDisk = CompositeDisk(InnerDisk, ShakuraSunyaevDisk, blackhole, mdot=mdot, Rmax=1e5)
+            analyticalDisk.solve()
+            odeDisk = CompositeDisk(InnerDiskODE, ShakuraSunyaevDisk, blackhole, mdot=mdot, Rmax=1e5)
+            odeDisk.solve()
+            np.testing.assert_almost_equal(odeDisk.H[1:] / analyticalDisk.H[1:], np.ones_like(odeDisk.R[1:]), decimal=1)
+            np.testing.assert_almost_equal(odeDisk.Mdot[1:] / analyticalDisk.Mdot[1:], np.ones_like(odeDisk.R[1:]), decimal=1)
+            np.testing.assert_almost_equal(odeDisk.Wrphi[1:] / analyticalDisk.Wrphi[1:], np.ones_like(odeDisk.R[1:]), decimal=1)
 
-    def testRsph(self):
+    def testDerivedRsphL(self):
         blackhole = CompactObject(M=10, a=0)
         mdot = 1000
-        disk = DiskWithOutflows(blackhole, mdot=mdot, alpha=0.1, N=50000, Rmax=100000)
-        disk.solve()
-
-        Rsph = disk.get_spherization_radius(disk.Qrad)
-
-        L = disk.L(Rsph)
-        self.assertAlmostEqual(L / blackhole.LEdd, 1, delta=0.1)
-        self.assertAlmostEqual(Rsph / blackhole.Risco / mdot, 1.62, delta=0.1)
+        for diskclass in [InnerDisk, InnerDiskODE]:
+            disk = CompositeDisk(diskclass, ShakuraSunyaevDisk, blackhole, mdot=mdot, N=50000, Rmax=1e5)
+            disk.solve()
+            L = disk.L(Rmin = disk.Rsph)
+            self.assertAlmostEqual(L / blackhole.LEdd, 1, delta=0.1, msg="L(R > Rsph) does not mach the Eddington luminosity!")
+            L = disk.outerDisk.L()
+            self.assertAlmostEqual(L / blackhole.LEdd, 1, delta=0.1, msg="The outer disk luminosity does not mach the Eddington luminosity!")
+            self.assertAlmostEqual(disk.Rsph / blackhole.Risco / mdot, 1.62, delta=0.1)
     
 
     def testEnergyConserved(self):
         """Test that the energy generation from the torque matches the radiative output Qrad"""
         mdot =10
         blackhole = CompactObject(M=10, a=0)
-        disk = DiskWithOutflows(blackhole, mdot=mdot, alpha=0.1)
-        disk.solve()
-        Q = - 3./4. * disk.Omega * disk.Wrphi
-        np.testing.assert_allclose(disk.Qrad / disk.Qrad, Q / disk.Qrad, rtol=1e-10)
+        for diskclass in [InnerDisk, InnerDiskODE]:
+            disk = CompositeDisk(diskclass, ShakuraSunyaevDisk, blackhole, mdot=mdot, Rmax=1e5, N=50000)
+            disk.solve()
+            Q = - 3./4. * disk.Omega * disk.Wrphi
+            np.testing.assert_allclose(disk.Qrad / disk.Qrad, Q / disk.Qrad, rtol=1e-10)
 
 
     def testQrad(self):
         """Test that inside Rsph the Qrad from the torque matches the expected analytical expression"""
         blackhole = CompactObject(M=10, a=0)
         mdot = 10
-        disk = DiskWithOutflows(blackhole, mdot=mdot, alpha=0.1, N=1000000, Rmax=100)
-        disk.solve()
-        deltaR = disk.R[1] - disk.R[0]
-        dMdR = np.gradient(disk.Mdot, deltaR)
-        Qrad = disk.Omega**2 * disk.R / (8 * np.pi) * dMdR
-        # avoid the edges where the numerical derivative is not good
-        np.testing.assert_allclose(np.ones_like(Qrad[disk.R < disk.Rsph])[1:-1], Qrad[disk.R < disk.Rsph][1:-1] / disk.Qrad[disk.R < disk.Rsph][1:-1],
-                                   rtol=1e-4)
+        for diskclass in [InnerDisk, InnerDiskODE]:
+            disk = CompositeDisk(diskclass, ShakuraSunyaevDisk, blackhole, mdot=mdot, Rmax=1e4, N=500000)
+            disk.solve()
+            dR = disk.R[1] - disk.R[0]
+            dMdR = np.gradient(disk.Mdot, dR)
+            Qrad = disk.Omega**2 * disk.R / (8 * np.pi) * dMdR
+            rrange = disk.R < disk.Rsph
+            # avoid the edges where the numerical derivative is not good
+            np.testing.assert_allclose(Qrad[rrange][1:-1] / disk.Qrad[rrange][1:-1], np.ones_like(Qrad[rrange])[1:-1],
+                                    rtol=0.1, err_msg="Failure in Qrad calculation!")
 
 if __name__ == '__main__':
     unittest.main()
